3eb6d79ee3db4cc756e17f5f0079282a87174e29
XThirteenIII <giuseppe.richetti@gmail.com>
2025-11-13 12:30:27 +0100

    add limit-crawler solution

--- 0-limit-crawler/main.go
+++ 0-limit-crawler/main.go
@@ -12,17 +12,21 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 // Crawl uses `fetcher` from the `mockfetcher.go` file to imitate a
 // real crawler. It crawls until the maximum depth has reached.
-func Crawl(url string, depth int, wg *sync.WaitGroup) {
+func Crawl(url string, depth int, wg *sync.WaitGroup, throttle <-chan time.Time) {
 	defer wg.Done()
 
 	if depth <= 0 {
 		return
 	}
 
+	// wait for throttle data every second, the <- blocks the Crawl function
+	// the first go routine that arrives here blocks the channel until it reads from it
+	<-throttle
 	body, urls, err := fetcher.Fetch(url)
 	if err != nil {
 		fmt.Println(err)
@@ -35,14 +39,16 @@ func Crawl(url string, depth int, wg *sync.WaitGroup) {
 	for _, u := range urls {
 		// Do not remove the `go` keyword, as Crawl() must be
 		// called concurrently
-		go Crawl(u, depth-1, wg)
+		go Crawl(u, depth-1, wg, throttle)
 	}
 }
 
 func main() {
 	var wg sync.WaitGroup
+	// This sends a Tick on the throttle channel (send only) every 1 second
+	throttle := time.Tick(1 * time.Second)
 
 	wg.Add(1)
-	Crawl("http://golang.org/", 4, &wg)
+	Crawl("http://golang.org/", 4, &wg, throttle)
 	wg.Wait()
 }
